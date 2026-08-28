6dc1ffd1966519736a1799ddfa3c3aac6d64dfdd
krss-94 <krss94@example.com>
2026-08-28 10:36:29 +0530

    V1-W4: widen MDT counter to 4 bits

--- Processor/Src/MicroArchConf.sv
+++ Processor/Src/MicroArchConf.sv
@@ -89,7 +89,7 @@ localparam CONF_BRANCH_GLOBAL_HISTORY_BIT_WIDTH = 10;   // Global history length
 
 // Memory dependency predictor
 localparam CONF_MDT_ENTRY_NUM = 1024;   // The number of prediction table entries.
-localparam CONF_MDT_COUNTER_BIT_WIDTH = 3;   // V1-W3: counter width per entry
+localparam CONF_MDT_COUNTER_BIT_WIDTH = 4;   // V1-W3: counter width per entry
 
 
 // --- D-cache
