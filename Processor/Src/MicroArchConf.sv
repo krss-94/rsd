fbac7c3f8ffcce6839e79f14475ba5a46d95b122
krss-94 <krss94@example.com>
2026-08-29 07:51:58 +0530

    Axis-A-CAP32: reduce table to 32 entries

--- Processor/Src/MicroArchConf.sv
+++ Processor/Src/MicroArchConf.sv
@@ -88,7 +88,7 @@ localparam CONF_PHT_ENTRY_NUM = 2048;
 localparam CONF_BRANCH_GLOBAL_HISTORY_BIT_WIDTH = 10;   // Global history length for g-share 
 
 // Memory dependency predictor
-localparam CONF_MDT_ENTRY_NUM = 64;   // Axis-A-CAP64: table capacity
+localparam CONF_MDT_ENTRY_NUM = 32;   // Axis-A-CAP32: table capacity
 
 
 // --- D-cache
